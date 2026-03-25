e952613d6fe0bc797c4a34ee75f757b2d99a2182
Saamson Stephen <saamson.2001@outlook.fr>
2026-03-25 19:35:06 +0100

    fix: force dotnet 8 with global.json

--- obj/Debug/net8.0/GestionStock.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestionStock.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionStock.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e349ebb26c1cd4bf3a4ac1efe443d43db1a617b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8513f2715e88e8e6fd41b0c2912cff7397aecee")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionStock.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionStock.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
